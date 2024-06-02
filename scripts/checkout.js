abdf22787c40e87f7d5616798dda4a8e40003460
mubinxyz <mubintp2003@gmail.com>
2024-06-02 12:29:23 +0330

    Delete products from cart when clicking delete

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,4 +1,4 @@
-import { cart } from "../data/cart.js";
+import { cart, removeFromCart } from "../data/cart.js";
 import { products } from "../data/products.js";
 import { formatCurrancy } from "./utils/money.js";
 
@@ -19,24 +19,23 @@ cart.forEach((cartItem) => {
   <div class="delivery-date">Delivery date: Tuesday, June 21</div>
 
   <div class="cart-item-details-grid">
-    <img
-      class="product-image"
-      src="${matchingProduct.image}"
-    />
+    <img class="product-image" src="${matchingProduct.image}" />
 
     <div class="cart-item-details">
-      <div class="product-name">
-        ${matchingProduct.name}
+      <div class="product-name">${matchingProduct.name}</div>
+      <div class="product-price">
+        $${formatCurrancy(matchingProduct.priceCents)}
       </div>
-      <div class="product-price">$${formatCurrancy(
-        matchingProduct.priceCents
-      )}</div>
       <div class="product-quantity">
-        <span> Quantity: <span class="quantity-label">${
-          cartItem.quantity
-        }</span> </span>
+        <span>
+          Quantity: <span class="quantity-label">${cartItem.quantity}</span>
+        </span>
         <span class="update-quantity-link link-primary"> Update </span>
-        <span class="delete-quantity-link link-primary"> Delete </span>
+        <span class="delete-quantity-link link-primary js-delete-link" data-product-id="${
+          matchingProduct.id
+        }">
+          Delete
+        </span>
       </div>
     </div>
 
@@ -83,3 +82,11 @@ cart.forEach((cartItem) => {
 });
 
 document.querySelector(".js-order-summary").innerHTML = cartSummaryHTML;
+
+document.querySelectorAll(".js-delete-link").forEach((link) => {
+  link.addEventListener("click", () => {
+    const productId = link.dataset.productId;
+    removeFromCart(productId);
+    console.log(cart);
+  });
+});
