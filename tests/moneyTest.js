1ca61edae7a03e6c86ec9f5242e8b8e2a08dea87
mubinxyz <mubintp2003@gmail.com>
2024-08-09 22:43:25 +0330

    create automated tests for formatCurrency

---
+++ tests/moneyTest.js
@@ -0,0 +1,31 @@
+import { formatCurrency } from "../scripts/utils/money.js";
+
+console.log("test suite: formatCurrency");
+
+console.info("converts cents into dollers");
+if (formatCurrency(2095) === "20.95") {
+  console.log("passed");
+} else {
+  console.log("failed");
+}
+
+console.info("works with 0");
+if (formatCurrency(0) === "0.00") {
+  console.log("passed");
+} else {
+  console.log("failed");
+}
+
+console.log("rounds up to the nearest cent");
+if (formatCurrency(2000.5) === "20.01") {
+  console.log("passed");
+} else {
+  console.log("failed");
+}
+
+console.log("it rounds up to the nearest cent");
+if (formatCurrency(2000.4) === "20.00") {
+  console.log("passed");
+} else {
+  console.log("failed");
+}
